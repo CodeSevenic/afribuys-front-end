33ec9b318154a7fa8fd8f31cd5f544a265d92094
CodeSevenic <sbu.audio.production@gmail.com>
2021-07-02 18:14:38 +0200

    code update

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
 import { useSelector } from 'react-redux';
 
@@ -6,12 +7,13 @@ import './SideMenu.css';
 
 const SideMenu = () => {
   const category = useSelector((state) => state.category);
+  const [open, setOpen] = useState(false);
 
   const renderCategories = (categories) => {
     let myCategories = [];
     for (let category of categories) {
       myCategories.push(
-        <li key={category.name}>
+        <li key={category.name} onClick={() => setOpen(!open)}>
           {category.parentId ? (
             <a
               href={`/${category.slug}?cid=${category._id}&type=${category.type}`}
@@ -22,7 +24,9 @@ const SideMenu = () => {
             <span>{category.name}</span>
           )}
           {category.children.length > 0 && (
-            <ul>{renderCategories(category.children)}</ul>
+            <ul className="supersubmenu">
+              {renderCategories(category.children)}
+            </ul>
           )}
         </li>
       );
@@ -30,36 +34,41 @@ const SideMenu = () => {
     return myCategories;
   };
 
+  console.log(renderCategories(category.categories));
+
+  const renderProductCat = () => {
+    return (
+      <>
+        <div className="sidemenu">
+          <div className="menu-navigation">
+            <ul className="menu-nav-tabs">
+              <li>Menu</li>
+              <li>Account</li>
+            </ul>
+          </div>
+          <div className="menu_tab">
+            <ul className="main-sub">
+              <li className="nav_product">
+                <span>Products</span>
+                <IoIosArrowForward className="Arrow_Forward" />
+                <ul className="submenu">
+                  {category.categories.length > 0
+                    ? renderCategories(category.categories)
+                    : null}
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   return (
-    <>
+    <div>
       <div className="back-overlay"></div>
-      <div className="sidemenu">
-        <div className="menu-navigation">
-          <ul className="menu-nav-tabs">
-            <li>Menu</li>
-            <li>Account</li>
-          </ul>
-        </div>
-        <div className="menu_tab">
-          <ul>
-            <li className="nav_product">
-              <span>Products</span>
-              <IoIosArrowForward className="Arrow_Forward" />
-            </li>
-            <li className="">
-              <span>Brands</span>
-            </li>
-            <li className="nav_specials">
-              <span>Special</span>
-              <IoIosArrowForward className="Arrow_Forward" />
-            </li>
-            <li className="">
-              <span>Promotion</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
+      {renderProductCat()}
+    </div>
   );
 };
 
