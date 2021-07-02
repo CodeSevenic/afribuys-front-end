8bebacb396358232adce680566641da755ac22b7
CodeSevenic <sbu.audio.production@gmail.com>
2021-07-02 15:01:07 +0200

    code update

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import MenuHeader from '../MenuHeader/MenuHeader';
+import SideMenu from '../SideMenu/SideMenu';
 
 const Layout = (props) => {
   return (
     <>
       <Header />
       <MenuHeader />
+      <SideMenu />
       {props.children}
     </>
   );
--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,43 +1,66 @@
 import React from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
+import { useSelector } from 'react-redux';
 
 import './SideMenu.css';
 
 const SideMenu = () => {
-  const renderMobileMenu = () => {
-    return (
-      <>
-        <div className="back-overlay"></div>
-        <div className="sidemenu">
-          <div className="menu-navigation">
-            <ul className="menu-nav-tabs">
-              <li>Menu</li>
-              <li>Account</li>
-            </ul>
-          </div>
-          <div className="menu_tab">
-            <ul>
-              <li className="nav_product">
-                <span>Products</span>
-                <IoIosArrowForward className="Arrow_Forward" />
-              </li>
-              <li className="">
-                <span>Brands</span>
-              </li>
-              <li className="nav_specials">
-                <span>Special</span>
-                <IoIosArrowForward className="Arrow_Forward" />
-              </li>
-              <li className="">
-                <span>Promotion</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </>
-    );
+  const category = useSelector((state) => state.category);
+
+  const renderCategories = (categories) => {
+    let myCategories = [];
+    for (let category of categories) {
+      myCategories.push(
+        <li key={category.name}>
+          {category.parentId ? (
+            <a
+              href={`/${category.slug}?cid=${category._id}&type=${category.type}`}
+            >
+              {category.name}
+            </a>
+          ) : (
+            <span>{category.name}</span>
+          )}
+          {category.children.length > 0 && (
+            <ul>{renderCategories(category.children)}</ul>
+          )}
+        </li>
+      );
+    }
+    return myCategories;
   };
-  return <div></div>;
+
+  return (
+    <>
+      <div className="back-overlay"></div>
+      <div className="sidemenu">
+        <div className="menu-navigation">
+          <ul className="menu-nav-tabs">
+            <li>Menu</li>
+            <li>Account</li>
+          </ul>
+        </div>
+        <div className="menu_tab">
+          <ul>
+            <li className="nav_product">
+              <span>Products</span>
+              <IoIosArrowForward className="Arrow_Forward" />
+            </li>
+            <li className="">
+              <span>Brands</span>
+            </li>
+            <li className="nav_specials">
+              <span>Special</span>
+              <IoIosArrowForward className="Arrow_Forward" />
+            </li>
+            <li className="">
+              <span>Promotion</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SideMenu;
