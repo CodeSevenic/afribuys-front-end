cc01fd879b1379ce54080bb28272535890b2f2b5
CodeSevenic <sbu.audio.production@gmail.com>
2021-07-09 12:18:21 +0200

    code update

--- src/containers/HomePage/HomeComponents.js
+++ src/containers/HomePage/HomeComponents.js
@@ -1,10 +1,59 @@
+import { useSelector } from 'react-redux';
 import './HomeComponent.css';
 
 export const HeadProductList = (props) => {
+  const category = useSelector((state) => state.category);
+
+  const renderCategories = (categories) => {
+    let myCategories = [];
+    for (let category of categories) {
+      myCategories.push(
+        <>
+          {category && (
+            <div className="menuCont" key={category.name}>
+              {category.children.length > 0 && (
+                <input type="checkbox" name="" id={category.name} />
+              )}
+
+              {category.parentId && !category.children.length > 0 ? (
+                <a
+                  className="labelText"
+                  href={`/${category.slug}?cid=${category._id}&type=${category.type}`}
+                >
+                  {category.name}
+                </a>
+              ) : (
+                <label htmlFor={category.name} className="listItemCont">
+                  {!category.parentId && (
+                    <img
+                      className="prodImgContainer"
+                      src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100"
+                      alt=""
+                    />
+                  )}
+                  <label htmlFor={category.name} className="labelText">
+                    {category.name}
+                  </label>
+                </label>
+              )}
+              {category.children.length > 0 && (
+                <ul>
+                  <li>{renderCategories(category.children)}</li>
+                </ul>
+              )}
+            </div>
+          )}
+        </>
+      );
+    }
+    return myCategories;
+  };
+
   return (
     <div>
-      <div>
-        {props.menuList && (
+      <div className="headProductList">
+        {renderCategories(category.categories)}
+        {/* {props.menuList && (
           <ul className="headProductList">
             {props.menuList.map((item, index) => (
               <li key={index}>
@@ -17,7 +66,7 @@ export const HeadProductList = (props) => {
               </li>
             ))}
           </ul>
-        )}
+        )} */}
       </div>
     </div>
   );
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -7,7 +7,8 @@ const HomePage = (props) => {
     <Layout>
       <div>
         <div>
-          <HeadProductList
+          <HeadProductList />
+          {/* <HeadProductList
             menuList={[
               {
                 label: 'Home & furniture',
@@ -30,7 +31,7 @@ const HomePage = (props) => {
                 src: `https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100`,
               },
             ]}
-          />
+          /> */}
         </div>
       </div>
     </Layout>
