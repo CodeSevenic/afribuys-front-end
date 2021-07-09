453f44f06260367e92ab63db32cc57955475f1ee
CodeSevenic <sbu.audio.production@gmail.com>
2021-07-09 07:07:56 +0200

    code update

--- src/containers/HomePage/HomeComponents.js
+++ src/containers/HomePage/HomeComponents.js
@@ -1,26 +1,18 @@
+import './HomeComponent.css';
+
 export const HeadProductList = (props) => {
   return (
     <div>
       <div>
         {props.menuList && (
-          <ul>
+          <ul className="headProductList">
             {props.menuList.map((item, index) => (
               <li key={index}>
-                <div>
-                  <a
-                    href={`${item?.href}`}
-                    onClick={(e) => {
-                      if (item.onClick) {
-                        e.preventDefault();
-                        item.onClick && item.onClick();
-                      }
-                    }}
-                  >
-                    <div>
-                      <img src={`${item.src}`} alt="" />
-                    </div>
-                    <p>{item.label}</p>
-                  </a>
+                <div className="listItemCont">
+                  <div className="prodImgContainer">
+                    <img src={`${item.src}`} alt="" />
+                  </div>
+                  <p className="labelText">{item.label}</p>
                 </div>
               </li>
             ))}
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -13,6 +13,22 @@ const HomePage = (props) => {
                 label: 'Home & furniture',
                 src: `https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100`,
               },
+              {
+                label: 'Home & furniture',
+                src: `https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100`,
+              },
+              {
+                label: 'Home & furniture',
+                src: `/images/1_bedroom_flat_in_johannesburg_central.jpg`,
+              },
+              {
+                label: 'Home & furniture',
+                src: `https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100`,
+              },
+              {
+                label: 'Home & furniture',
+                src: `https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100`,
+              },
             ]}
           />
         </div>
