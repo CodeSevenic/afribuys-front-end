94b3f9add933f9d36df08920a2e4ab07e6d83b7c
CodeSevenic <sbu.audio.production@gmail.com>
2021-07-10 21:56:04 +0200

    code update

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import MenuHeader from '../MenuHeader/MenuHeader';
+import './Layout.css';
 
 const Layout = (props) => {
   return (
     <>
       <Header />
       {!props.disableSubHeader && <MenuHeader />}
-      {props.children}
+      <main className="mainView">{props.children}</main>
       <Footer />
     </>
   );
