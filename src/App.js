92820f1a07dbe266629223e0eca8d56e7cc0866d
CodeSevenic <sbu.audio.production@gmail.com>
2021-08-07 16:41:22 +0200

    code update

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ function App() {
           <Route path="/" exact component={HomePage} />
           <Route path="/cart" component={CartPage} />
           <Route path="/checkout" component={CheckoutPage} />
-          <Route path="/account/orders" component={OrderPage} />
-          {/* <Route path="/order_details/:orderId" component={OrderDetailsPage} /> */}
+          {/* <Route path="/account/orders" component={OrderPage} /> */}
+          <Route path="/order_details/:orderId" component={OrderDetailsPage} />
           <Route
             path="/:productSlug/:productId/p"
             component={ProductDetailsPage}
